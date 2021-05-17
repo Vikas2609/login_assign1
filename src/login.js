798369d20cd3288f0e034fa1cff1588e13a2353b
Vikas2609 <vikasml.2609@gmail.com>
2021-05-17 12:54:49 +0530

    state

--- src/login.js
+++ src/login.js
@@ -3,16 +3,41 @@ import styles from './login.modules.css'
 
 
 class MyForm extends React.Component {
-  
+  constructor(props){
+    super(props);
+    this.state={username: "",
+                password: ""
+               }
+  }
+
+  handleUserChange= (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  handlePassChange= (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.username);
+    console.log(this.state.password);
+  }
+
   render() {
     return (
   <div>
      <div><h1 className='heading'>LOGIN PAGE</h1></div>
       
      <div className='form'>
-      <form>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="username" value={this.state.username} onChange={this.handleUserChange}/>
+        <input type="password" placeholder="password" value={this.state.password} onChange={this.handlePassChange}/>
         <button>login</button>
       </form>
     </div>
